de71a7864ea1e143bd31812e6b89e1a4207195b4
Garrett <Garv5014@gmail.com>
2021-04-19 13:27:13 -0600

    hope it works

---
+++ Resume_Bootstrap/resumeCode.js
@@ -0,0 +1,225 @@
+
+// @media screen and (min-width: 45em ){
+//     /* grid layout */
+// #layout{
+//     display: grid;
+//     /* rows/ columns */
+//     grid:  5em 1fr .25fr .25fr 15em  / 1fr 1fr  ;
+//     grid-template-areas: 
+//     "header header"
+//     "sum    emp"
+//     "edu    vol "
+//     "skills vol"
+//     "ref    ref"
+//     ;
+// }
+// /* grid names */
+// .header{
+//     grid-area: header ;
+// }
+// .header > *{
+//     grid-area: header;
+// }
+
+// .Summary{
+//     grid-area: sum;
+// }
+
+// .Employment{
+//     grid-area: emp;
+// }
+
+// .Education{
+//     grid-area: edu;
+// }
+
+// .Skills{
+//     grid-area: skills;
+// }
+
+// .Volunteering{
+//     grid-area: vol;
+
+// }
+
+// .References{
+//     grid-area: ref;
+// }
+
+// /* body */
+// body {
+//     font-family: Georgia, 'Times New Roman', Times, sans-serif;
+//     font-size: medium;
+//     margin-right: 2em; 
+//     margin-left: 2em; 
+//     margin-top: 1em;
+//     max-height: 11.5in;
+//     max-width: 8.5;
+// }
+// img{
+//    display: inline-grid ;
+//    max-width: 60%;
+//    max-height: 50%;
+// }
+// }
+
+
+// /* layout for 3/4 screen */
+// @media screen and (min-width: 60em ){
+// /* grid layout */
+// #layout{
+//     display: grid;
+//     /* rows/ columns */
+//     grid:  5em 35em .25fr .25fr 15em  / 1fr 1fr  ;
+//     grid-template-areas: 
+//     "header header"
+//     "sum    emp"
+//     "edu    vol "
+//     "skills vol"
+//     "ref    ref"
+//     ;
+// }
+// /* grid names */
+// .header{
+//     grid-area: header ;
+// }
+// .header > *{
+//     grid-area: header;
+// }
+
+// .Summary{
+//     display: grid;
+//     grid-area: sum;
+//     grid:  .25fr 1fr /1fr 1fr;
+//     grid-template-areas: 
+//     "sumHead sumHead"
+//     "image para"
+//     ;
+// }
+// .sumHead{
+// grid-area: sumHead;
+// }
+// .image{
+//     grid-area: image;
+//     display: inline;
+//     max-width:  100%;
+//     max-height: 60%;
+// }
+// .para{
+//     display: inline;
+//     grid-area: para;
+//     font-size: large ;
+// }
+
+// .Employment{
+//     grid-area: emp;
+// }
+
+// .Education{
+//     grid-area: edu;
+// }
+
+// .Skills{
+//     grid-area: skills;
+// }
+
+// .Volunteering{
+//     grid-area: vol;
+
+// }
+
+// .References{
+//     grid-area: ref;
+// }
+
+// /* body */
+// body {
+//     font-family: Georgia, 'Times New Roman', Times, sans-serif;
+//     font-size: medium;
+//     margin-right: 5%; 
+//     margin-left: 5%; 
+//     margin-top: 1em;
+//     max-height: 11.5in;
+//     max-width: 8.5;
+// }
+// }
+
+// /* layout for full screen */
+// @media screen and (min-width: 80em ){
+// /* grid layout */
+// #layout{
+//     display: grid;
+//     /* rows/ columns */
+//     grid:  5em 28em .25fr .25fr 15em  / 1fr 1fr  ;
+//     grid-template-areas: 
+//     "header header"
+//     "sum    emp"
+//     "edu    vol "
+//     "skills vol"
+//     "ref    ref"
+//     ;
+// }
+// /* grid names */
+// .header{
+//     grid-area: header ;
+// }
+// .header > *{
+//     grid-area: header;
+// }
+
+// .Summary{
+//     display: grid;
+//     grid-area: sum;
+//     grid:  .25fr 1fr /1fr 1fr;
+//     grid-template-areas: 
+//     "sumHead sumHead"
+//     "image para"
+//     ;
+// }
+// .sumHead{
+// grid-area: sumHead;
+// }
+// .image{
+//     grid-area: image;
+//     display: inline;
+//     max-width:  100%;
+//     max-height: 80%;
+// }
+// .para{
+//     display: inline;
+//     grid-area: para;
+//     font-size: large ;
+// }
+
+// .Employment{
+//     grid-area: emp;
+// }
+
+// .Education{
+//     grid-area: edu;
+// }
+
+// .Skills{
+//     grid-area: skills;
+// }
+
+// .Volunteering{
+//     grid-area: vol;
+
+// }
+
+// .References{
+//     grid-area: ref;
+// }
+
+// /* body */
+// body {
+//     font-family: Georgia, 'Times New Roman', Times, sans-serif;
+//     font-size: medium;
+//     margin-right: 5%; 
+//     margin-left: 5%; 
+//     margin-top: 1em;
+//     max-height: 11.5in;
+//     max-width: 8.5;
+// }
+
